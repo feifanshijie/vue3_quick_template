2dafcd510c7c0e8eebc3ef62655030a5a8ec456a
非帆 <2652525544@qq.com>
2021-12-20 18:11:37 +0800

    fixbug

--- src/pages/home/welcome.vue
+++ src/pages/home/welcome.vue
@@ -3,32 +3,8 @@
 </template>
 <script>
 import {defineComponent, onMounted, reactive, ref, h} from "vue";
-import {
-  NBreadcrumb,
-  NBreadcrumbItem,
-  NIcon,
-  NButton,
-  NForm,
-  NFormItem,
-  NInput,
-  NInputNumber,
-  NDataTable,
-  NSpin,
-  NPopconfirm,
-  NModal,
-  NSpace,
-  useDialog,
-  NSelect,
-  useMessage,
-  NCheckbox, NTag,
-  NInputGroup
-} from "naive-ui";
-import {WebsiteList, WebsiteCreate, WebsiteDelete, WebsiteBatchDelete} from "@apis/website";
-import {HomeOutline, StarOutline, StarSharp} from '@vicons/ionicons5'
-import {ColorConfig, IconConfig, IconImageConfig} from "../../configs/business";
 // @ts-ignore
 const StarIcon = 'assets/star_select.gif';
-
 export default defineComponent({
   components: {
 
